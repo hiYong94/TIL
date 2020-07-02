4f34a311677e8dde0bdf50b301d4d244c3ece95f
hiYong94 <chl9741@naver.com>
2020-07-02 23:23:13 +0900

    Add '그리드 구현하기'

---
+++ inflearn/boilerplate-mern-stack-master/client/src/components/views/commons/GridCards.js
@@ -0,0 +1,18 @@
+// rfce functional component
+import React from 'react'
+import { Col } from 'antd'
+
+function GridCards(props) {
+    return (
+        // 전체사이즈가 24
+        <Col lg={6} md={8} xs={24}>
+            <div style={{ position: 'relative' }}>
+                <a href={`/movie/${props.movieId}`}>
+                    <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.movieName}/>
+                </a>
+            </div>
+        </Col>
+    )
+}
+
+export default GridCards
