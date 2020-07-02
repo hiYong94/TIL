2ff7c3d8ecaabad20686653da6e2e196607cdee1
hiYong94 <chl9741@naver.com>
2020-07-02 22:59:44 +0900

    Add '랜딩페이지의 메인 이미지 만들기'

--- inflearn/boilerplate-mern-stack-master/client/src/components/Config.js
+++ inflearn/boilerplate-mern-stack-master/client/src/components/Config.js
@@ -6,3 +6,6 @@ export const API_URL = 'https://api.themoviedb.org/3/'
 
 // TheMovieDB Image URL
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/'
+
+// Movie App API key
+export const API_KEY = '63ddf626b0f73fdb51b76cd5d7a1832e'
